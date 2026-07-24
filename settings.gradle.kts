4eb160c5e7438f5b3d54e04d75528bf72dec146c
theEvilReaper <theevilreaper@users.noreply.github.com>
2026-07-24 13:05:07 +0200

    fix(deps): update cyano to 0.7.2

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -23,7 +23,7 @@ dependencyResolutionManagement {
         create("libs") {
             version("minestom", "2026.06.20-26.1.2")
             version("junit", "6.1.0")
-            version("cyano", "0.6.3")
+            version("cyano", "0.7.2")
 
             library("minestom","net.minestom", "minestom").versionRef("minestom")
             library("cyano", "net.onelitefeather", "cyano").versionRef("cyano")
